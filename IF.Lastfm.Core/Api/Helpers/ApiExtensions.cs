67e98ad5bb61f45a372f66ce54102a9150dd464a
Rikki Tooley <hello@rikk.it>
2013-06-13 17:27:51 +0100

    Started UserApi

---
+++ IF.Lastfm.Core/Api/Helpers/ApiExtensions.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace IF.Lastfm.Core.Api.Helpers
+{
+    public static class ApiExtensions
+    {
+        public static string GetApiName(this Enum en)
+        {
+            var type = en.GetType();
+
+            var memInfo = type.GetMember(en.ToString());
+
+            if (memInfo.Length > 0)
+            {
+                var attrs = memInfo[0].GetCustomAttributes(typeof (ApiNameAttribute), false);
+
+                if (attrs != null && attrs.Length > 0)
+                {
+                    return ((ApiNameAttribute) attrs[0]).Text;
+                }
+            }
+
+            return en.ToString();
+        }
+    }
+}
---
+++ IF.Lastfm.Core/Api/Helpers/ApiNameAttribute.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace IF.Lastfm.Core.Api.Helpers
+{
+    public class ApiNameAttribute : Attribute
+    {
+        public string Text { get; private set; }
+
+        public ApiNameAttribute(string name)
+        {
+            Text = name;
+        }
+    }
+}
