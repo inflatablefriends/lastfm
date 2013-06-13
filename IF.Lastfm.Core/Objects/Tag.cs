444df4bddb304c9a312d413d041299cac0be4cec
Rikki Tooley <hello@rikk.it>
2013-06-13 16:29:52 +0100

    Tag and track parsing

--- IF.Lastfm.Core/Objects/Tag.cs
+++ IF.Lastfm.Core/Objects/Tag.cs
@@ -1,6 +1,25 @@
-﻿namespace IF.Lastfm.Core.Objects
+﻿using System;
+using Newtonsoft.Json.Linq;
+
+namespace IF.Lastfm.Core.Objects
 {
     public class Tag
     {
+        #region Properties
+
+        public string Name { get; set; }
+        public Uri Url { get; set; }
+
+        #endregion
+
+        public static Tag ParseJToken(JToken token)
+        {
+            var t = new Tag();
+
+            t.Name = token.Value<string>("name");
+            t.Url = new Uri(token.Value<string>("url"), UriKind.Absolute);
+
+            return t;
+        }
     }
 }
