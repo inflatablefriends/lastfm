d6c15cfe59c426bf48a27e867ec4b59b3779bbae
Rikki Tooley <hello@rikk.it>
2013-07-01 23:18:06 +0100

    Get artist info command

--- IF.Lastfm.Core/Objects/Artist.cs
+++ IF.Lastfm.Core/Objects/Artist.cs
@@ -1,9 +1,54 @@
-﻿using Newtonsoft.Json.Linq;
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using IF.Lastfm.Core.Api.Helpers;
+using Newtonsoft.Json.Linq;
 
 namespace IF.Lastfm.Core.Objects
 {
+    /// <summary>
+    /// Todo bio, tour, similar, stats, streamable
+    /// </summary>
     public class Artist
     {
+        #region Properties
+
+        public string Name { get; set; }
+        public string Mbid { get; set; }
+        public Uri Url { get; set; }
+        public bool OnTour { get; set; }
+        public IEnumerable<Tag> Tags { get; set; }
+        public LastImageCollection Images { get; set; }
+
+        #endregion
+
+        internal static Artist ParseJToken(JToken token)
+        {
+            var a = new Artist();
+
+            a.Name = token.Value<string>("name");
+            a.Mbid = token.Value<string>("mbid");
+            a.Url = new Uri(token.Value<string>("url"), UriKind.Absolute);
+
+            a.OnTour = Convert.ToBoolean(token.Value<int>("ontour"));
+            
+            var tagsToken = token.SelectToken("tags");
+            if (tagsToken != null)
+            {
+                a.Tags = tagsToken.SelectToken("tag").Children().Select(Tag.ParseJToken);
+            }
+
+            var images = token.SelectToken("image");
+            if (images != null)
+            {
+                var imageCollection = LastImageCollection.ParseJToken(images);
+                a.Images = imageCollection;
+            }
+            
+            return a;
+        }
+
         internal static string GetNameFromJToken(JToken artistToken)
         {
             var name = artistToken.Value<string>("name");
