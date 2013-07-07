1e18e999bfaef1406df1bae7d695a9a8a0f82a43
Rikki Tooley <hello@rikk.it>
2013-07-07 16:16:54 +0100

    GetShoutsCommand

--- IF.Lastfm.Core/Api/ITrackApi.cs
+++ IF.Lastfm.Core/Api/ITrackApi.cs
@@ -1,7 +1,7 @@
 ﻿using System.Collections.Generic;
 using System.Threading.Tasks;
 using IF.Lastfm.Core.Api.Helpers;
-using xBrainLab.Security.Cryptography;
+using IF.Lastfm.Core.Objects;
 
 namespace IF.Lastfm.Core.Api
 {
@@ -11,5 +11,13 @@ namespace IF.Lastfm.Core.Api
 
         Task<LastResponse> ScrobbleAsync(Scrobble scrobble);
         Task<LastResponse> ScrobbleAsync(IEnumerable<Scrobble> scrobble);
+
+        Task<PageResponse<Shout>> GetShoutsForTrackAsync(string trackname, string artistname,
+                                              int page = 0,
+                                              int count = LastFm.DefaultPageLength);
+
+        Task<PageResponse<Shout>> GetShoutsForTrackWithMbidAsync(string mbid,
+                                              int page = 0,
+                                              int count = LastFm.DefaultPageLength);
     }
 }
--- IF.Lastfm.Core/Objects/Shout.cs
+++ IF.Lastfm.Core/Objects/Shout.cs
@@ -1,6 +1,40 @@
-﻿namespace IF.Lastfm.Core.Objects
+﻿using System;
+using System.Globalization;
+using Newtonsoft.Json.Linq;
+
+namespace IF.Lastfm.Core.Objects
 {
     public class Shout
     {
+        #region Properties
+
+        public string Body { get; set; }
+        public string Author { get; set; }
+        public DateTime TimePosted { get; set; }
+
+        #endregion
+
+        public static Shout ParseJToken(JToken token)
+        {
+            var s = new Shout();
+
+            s.Body = token.Value<string>("body");
+            s.Author = token.Value<string>("author");
+
+
+            var provider = CultureInfo.InvariantCulture;
+
+            var date = token.Value<string>("date");
+            DateTime time;
+            // Tue, 18 Jun 2013 17:39:50
+            var success = DateTime.TryParseExact(date, "ddd, dd MMM yyyy HH:mm:ss", provider, DateTimeStyles.AssumeUniversal, out time);
+
+            if (success)
+            {
+                s.TimePosted = time;
+            }
+
+            return s;
+        }
     }
 }
