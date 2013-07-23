ec0954385ddb08fad02a9efda8200ee581b816e9
Rikki Tooley <hello@rikk.it>
2013-07-23 15:48:03 +0100

    Tidying user.getTopAlbums call.

--- IF.Lastfm.Core/Api/IUserApi.cs
+++ IF.Lastfm.Core/Api/IUserApi.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Threading.Tasks;
 using IF.Lastfm.Core.Api.Enums;
 using IF.Lastfm.Core.Api.Helpers;
@@ -11,14 +10,16 @@ namespace IF.Lastfm.Core.Api
     {
         IAuth Auth { get; }
 
-        Task<PageResponse<Album>> GetTopAlbums(LastStatsTimeSpan span,
-                                              int startIndex = 0,
-                                              int endIndex = LastFm.DefaultPageLength);
+        Task<PageResponse<Album>> GetTopAlbums(string username,
+            LastStatsTimeSpan span,
+            int startIndex = 0,
+            int endIndex = LastFm.DefaultPageLength);
 
         Task<PageResponse<Track>> GetRecentScrobbles(string username, DateTime since,
-                                              int startIndex = 0,
-                                              int endIndex = LastFm.DefaultPageLength);
+            int startIndex = 0,
+            int endIndex = LastFm.DefaultPageLength);
 
-        Task<PageResponse<Station>> GetRecentStations(int pagenumber, int count = LastFm.DefaultPageLength);
+        Task<PageResponse<Station>> GetRecentStations(int pagenumber,
+            int count = LastFm.DefaultPageLength);
     }
 }
