89732922da15f8a6c5a947daf1e95b3d7b073f3a
Rikki Tooley <rikki@inflatablefriends.co.uk>
2014-11-16 19:20:54 +0000

    Setting up OpenCover and ReportGenerator tools Closes #8

---
+++ coverage.ps1
@@ -0,0 +1,28 @@
+﻿$outputDir = ".\tmp"
+
+if (!(Test-Path $outputDir))
+{
+    New-Item -ItemType Directory -Path $outputDir
+}
+
+# OpenCover
+$openCover = ".\packages\OpenCover.4.5.3207\OpenCover.Console.exe"
+$testlib = "C:\Program Files (x86)\Microsoft Visual Studio 12.0\Common7\IDE\MSTest.exe"
+$testdll = ".\src\IF.Lastfm.Core.Tests\bin\Debug\IF.Lastfm.Core.Tests.dll"
+$testargs = "/testcontainer:$testdll"
+$output = "$outputDir\coverage.xml"
+
+$openCoverCommand = "$openCover -register:user -mergebyhash `"-target:$testlib`" `"-targetargs:$testargs`" -output:$output"
+
+# ReportGenerator
+$repgen = ".\packages\ReportGenerator.2.0.1.0\ReportGenerator.exe"
+$reportOutputDir = "$outputDir\report"
+$repgenCommand = "$repgen -reports:`"$output`" -targetDir:`"$reportOutputDir`""
+
+# --------
+
+echo "Running OpenCover...\n"
+iex $openCoverCommand
+
+echo "\nGenerating report... \n"
+iex $repgenCommand
