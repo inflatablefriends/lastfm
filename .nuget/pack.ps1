8246fcb609c706d424eb7bb16b4ae06e156479b9
Rikki Tooley <rikki@inflatablefriends.co.uk>
2014-11-21 00:01:39 +0000

    Only use semantic versioning when packaging NuGet

--- .nuget/pack.ps1
+++ .nuget/pack.ps1
@@ -1,12 +1,24 @@
+param(
+    [string] $versionSuffix
+)
+
 $root = (split-path -parent $MyInvocation.MyCommand.Definition) + '\..'
 $version = [System.Reflection.Assembly]::LoadFile("$root\src\IF.Lastfm.Core\bin\Release\IF.Lastfm.Core.dll").GetName().Version
-$versionStr = $version.ToString()
+
+if ($versionSuffix)
+{
+    $versionStr = "{0}.{1}.{2}-{3}" -f ($version.Major, $version.Minor, $version.Build, $versionSuffix)
+}
+else
+{
+    $versionStr = $version.ToString()
+}
 
 Write-Host "Setting .nuspec version tag to $versionStr"
 
 $content = (Get-Content $root\.nuget\IF.Lastfm.nuspec) 
 $content = $content -replace '\$version',$versionStr
 
-$content | Out-File $root\.nuget\IF.Lastfm.compiled.nuspec
+$content | Out-File $root\.nuget\IF.Lastfm.$versionStr.compiled.nuspec
 
-& $root\.nuget\NuGet.exe pack $root\.nuget\IF.Lastfm.compiled.nuspec
+& $root\.nuget\NuGet.exe pack $root\.nuget\IF.Lastfm.$versionStr.compiled.nuspec
