e5395dfd19cd0f27d77c6d66acf821e619ee6eef
Harry <harry@zumicts.com>
2014-12-14 18:31:36 -0400

    Add bio and similar to artist object

---
+++ src/IF.Lastfm.Core/Objects/LastBio.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
+
+namespace IF.Lastfm.Core.Objects
+{
+    public class LastBio : ILastfmObject
+    {
+        #region Properties
+
+        public DateTime Published { get; set; }
+        public string Summary { get; set; }
+        public string Content { get; set; }
+        public int YearFormed { get; set; }
+
+        #endregion
+
+        internal static LastBio ParseJToken(JToken token)
+        {
+            return new LastBio
+            {
+                Published = token.Value<DateTime>("published"),
+                Summary = token.Value<string>("summary").Trim(),
+                Content = token.Value<string>("content").Trim(),
+                YearFormed = token.Value<int>("yearformed")
+            };
+        }
+    }
+}
