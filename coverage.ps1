923db053cf7a3cc1bd8b0a6eb2e7071ddb2eda9b
Rikki Tooley <rikki@inflatablefriends.co.uk>
2015-01-04 04:04:58 +0000

    Include integration tests in code coverage report

--- coverage.ps1
+++ coverage.ps1
@@ -9,7 +9,8 @@ if (!(Test-Path $outputDir))
 $openCover = ".\packages\OpenCover.4.5.3207\OpenCover.Console.exe"
 $testlib = "C:\Program Files (x86)\Microsoft Visual Studio 12.0\Common7\IDE\MSTest.exe"
 $testdll = ".\src\IF.Lastfm.Core.Tests\bin\Debug\IF.Lastfm.Core.Tests.dll"
-$testargs = "/testcontainer:$testdll"
+$integrationTestDll = ".\src\IF.Lastfm.Core.Tests.Integration\bin\Debug\IF.Lastfm.Core.Tests.Integration.dll"
+$testargs = "/testcontainer:$testdll /testcontainer:$integrationTestDll "
 $output = "$outputDir\coverage.xml"
 
 $openCoverCommand = "$openCover -register:user -mergebyhash `"-target:$testlib`" `"-targetargs:$testargs`" -output:$output"
