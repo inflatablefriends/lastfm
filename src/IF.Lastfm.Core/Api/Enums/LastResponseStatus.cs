aca940e25c3d05d07bedc59fb9d21803984bb028
kasthack <kasthack@epicm.org>
2015-04-05 00:05:19 +0300

    LastResponseStatus parsing

--- src/IF.Lastfm.Core/Api/Enums/LastResponseStatus.cs
+++ src/IF.Lastfm.Core/Api/Enums/LastResponseStatus.cs
@@ -22,72 +22,72 @@
         /// <summary>
         /// The service requested does not exist (2)
         /// </summary>
-        BadService,
+        BadService = 2,
 
         /// <summary>
         /// The method requested does not exist in this service (3)
         /// </summary>
-        BadMethod,
+        BadMethod = 3,
 
         /// <summary>
         /// This credential does not have permission to access the service requested (4)
         /// </summary>
-        BadAuth,
+        BadAuth = 4,
 
         /// <summary>
         /// This service doesn't exist in the requested format
         /// </summary>
-        BadFormat,
+        BadFormat = 5,
 
         /// <summary>
         /// Required parameters were missing from the request (6)
         /// </summary>
-        MissingParameters,
+        MissingParameters = 6,
 
         /// <summary>
         /// The requested resource is invalid (7)
         /// </summary>
-        BadResource,
+        BadResource = 7,
 
         /// <summary>
         /// An unknown failure occured when creating the response (8)
         /// </summary>
-        Failure,
+        Failure = 8,
 
         /// <summary>
         /// The session has expired, reauthenticate before retrying (9)
         /// </summary>
-        SessionExpired,
+        SessionExpired = 9,
 
         /// <summary>
         /// The provided API key was invalid (10)
         /// </summary>
-        BadApiKey,
+        BadApiKey = 10,
 
         /// <summary>
         /// This service is temporarily offline, retry later (11)
         /// </summary>
-        ServiceDown,
+        ServiceDown = 11,
 
         /// <summary>
         /// The request signature was invalid. Check that your API key and secret are valid. (13)
         /// You can generate new keys at http://www.last.fm/api/accounts
         /// </summary>
-        BadMethodSignature,
+        BadMethodSignature = 13,
 
         /// <summary>
         /// There was a temporary error while processing the request, retry later (16)
         /// </summary>
-        TemporaryFailure,
+        TemporaryFailure = 16,
 
         /// <summary>
         /// This API key has been suspended, please generate a new key at http://www.last.fm/api/accounts (26)
         /// </summary>
-        KeySuspended,
+        KeySuspended = 26,
 
         /// <summary>
         /// This API key has been rate-limited because too many requests have been made in a short period. Retry later (29)
         /// </summary>
-        RateLimited
+        RateLimited = 29
     }
 }
