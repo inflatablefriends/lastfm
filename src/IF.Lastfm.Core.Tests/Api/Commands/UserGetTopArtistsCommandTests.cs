23be0352572ac11ee2dc550d3a795367bc50cb7d
Piotr Wiewiura <piotr.wiewiura@gmail.com>
2015-06-27 22:30:18 +0200

    Implemented user.getTopArtists

---
+++ src/IF.Lastfm.Core.Tests/Api/Commands/UserGetTopArtistsCommandTests.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using IF.Lastfm.Core.Api.Commands.User;
+using IF.Lastfm.Core.Api.Enums;
+using IF.Lastfm.Core.Api.Helpers;
+using IF.Lastfm.Core.Objects;
+using IF.Lastfm.Core.Tests.Resources;
+using NUnit.Framework;
+
+namespace IF.Lastfm.Core.Tests.Api.Commands
+{
+    public class UserGetTopArtistsCommandTests : CommandTestsBase
+    {
+        private const string USER = "Aurvandil";
+        private const LastStatsTimeSpan SPAN = LastStatsTimeSpan.Overall;
+
+        [Test]
+        public void ParametersCorrect()
+        {
+            var expected = new Dictionary<string, string>
+            {
+                {"user", USER},
+                {"period", SPAN.GetApiName()},
+                {"page", "5"},
+                {"limit", "10"}
+            };
+
+            var command = new GetTopArtistsCommand(MAuth.Object, USER, SPAN)
+            {
+                Page = 5,
+                Count = 10
+            };
+
+            command.SetParameters();
+            command.Parameters.Remove("disablecachetoken");
+
+            TestHelper.AssertSerialiseEqual(expected, command.Parameters);
+        }
+
+        [Test]
+        public async Task HandleResponseSingle()
+        {
+            var command = new GetTopArtistsCommand(MAuth.Object, USER, SPAN)
+            {
+                Page = 1,
+                Count = 1
+            };
+
+            command.SetParameters();
+
+            var expectedArtist = new LastArtist
+            {
+                Name = "Anathema",
+                PlayCount = 5216,
+                Mbid = "20aa23e3-3532-42ca-acf6-e8c2e9df2688",
+                Url = new Uri("http://www.last.fm/music/Anathema"),
+                MainImage =
+                    new LastImageSet("http://userserve-ak.last.fm/serve/34/12571597.jpg",
+                        "http://userserve-ak.last.fm/serve/64/12571597.jpg",
+                        "http://userserve-ak.last.fm/serve/126/12571597.jpg",
+                        "http://userserve-ak.last.fm/serve/252/12571597.jpg",
+                        "http://userserve-ak.last.fm/serve/_/12571597/Anathema+Judgement+promo.jpg")
+            };
+
+            var response = CreateResponseMessage(Encoding.UTF8.GetString(UserApiResponses.UserGetTopArtistsSingle));
+            var parsed = await command.HandleResponse(response);
+
+            Assert.IsTrue(parsed.Success);
+            Assert.AreEqual(1, parsed.Page);
+            Assert.AreEqual(1, parsed.PageSize);
+            Assert.AreEqual(1124, parsed.TotalItems);
+            Assert.AreEqual(1124, parsed.TotalPages);
+            Assert.AreEqual(1, parsed.Content.Count);
+
+            var actualArtist = parsed.Content.First();
+
+            TestHelper.AssertSerialiseEqual(expectedArtist, actualArtist);
+        }
+
+        [Test]
+        public async Task HandleResponseMultiple()
+        {
+            var command = new GetTopArtistsCommand(MAuth.Object, USER, SPAN)
+            {
+                Page = 1,
+                Count = 2
+            };
+
+            command.SetParameters();
+
+            var expectedArtists = new List<LastArtist>
+            {
+                new LastArtist
+                {
+                    Name = "Anathema",
+                    PlayCount = 5216,
+                    Mbid = "20aa23e3-3532-42ca-acf6-e8c2e9df2688",
+                    Url = new Uri("http://www.last.fm/music/Anathema"),
+                    MainImage =
+                        new LastImageSet("http://userserve-ak.last.fm/serve/34/12571597.jpg",
+                            "http://userserve-ak.last.fm/serve/64/12571597.jpg",
+                            "http://userserve-ak.last.fm/serve/126/12571597.jpg",
+                            "http://userserve-ak.last.fm/serve/252/12571597.jpg",
+                            "http://userserve-ak.last.fm/serve/_/12571597/Anathema+Judgement+promo.jpg")
+                },
+                new LastArtist
+                {
+                    Name = "Insomnium",
+                    PlayCount = 4670,
+                    Mbid = "c1f8e226-75ea-4fe6-83ce-59c122bcbca4",
+                    Url = new Uri("http://www.last.fm/music/Insomnium"),
+                    MainImage =
+                        new LastImageSet("http://userserve-ak.last.fm/serve/34/70409268.jpg",
+                            "http://userserve-ak.last.fm/serve/64/70409268.jpg",
+                            "http://userserve-ak.last.fm/serve/126/70409268.jpg",
+                            "http://userserve-ak.last.fm/serve/252/70409268.jpg",
+                            "http://userserve-ak.last.fm/serve/500/70409268/Insomnium.jpg")
+                },
+            };
+
+            var response = CreateResponseMessage(Encoding.UTF8.GetString(UserApiResponses.UserGetTopArtistsMultiple));
+            var parsed = await command.HandleResponse(response);
+
+            Assert.IsTrue(parsed.Success);
+            Assert.AreEqual(1, parsed.Page);
+            Assert.AreEqual(2, parsed.PageSize);
+            Assert.AreEqual(1124, parsed.TotalItems);
+            Assert.AreEqual(562, parsed.TotalPages);
+            Assert.AreEqual(2, parsed.Content.Count);
+
+            var actualArtists = parsed.Content;
+
+            TestHelper.AssertSerialiseEqual(expectedArtists, actualArtists);
+        }
+
+        [Test]
+        public async Task HandleResponseEmpty()
+        {
+            const string USER_WITH_NO_PLAYS = "e";
+            var command = new GetTopArtistsCommand(MAuth.Object, USER_WITH_NO_PLAYS, SPAN)
+            {
+                Page = 1,
+                Count = 1
+            };
+
+            command.SetParameters();
+
+            var response = CreateResponseMessage(Encoding.UTF8.GetString(UserApiResponses.UserGetTopArtistsEmpty));
+            var parsed = await command.HandleResponse(response);
+
+            Assert.IsTrue(parsed.Success);
+            Assert.AreEqual(1, parsed.Page);
+            Assert.AreEqual(0, parsed.PageSize);
+            Assert.AreEqual(0, parsed.TotalItems);
+            Assert.AreEqual(1, parsed.TotalPages);
+            Assert.AreEqual(0, parsed.Content.Count);
+        }
+
+        [Test]
+        public async Task HandleResponseError()
+        {
+            var command = new GetTopArtistsCommand(MAuth.Object, USER, SPAN)
+            {
+                Page = 1,
+                Count = 1
+            };
+
+            command.SetParameters();
+
+            var response = CreateResponseMessage(Encoding.UTF8.GetString(UserApiResponses.UserGetTopArtistsError));
+            var parsed = await command.HandleResponse(response);
+
+            Assert.IsFalse(parsed.Success);
+            Assert.AreEqual(1, parsed.Page);
+            Assert.AreEqual(0, parsed.PageSize);
+            Assert.AreEqual(0, parsed.TotalItems);
+            Assert.AreEqual(1, parsed.TotalPages);
+            Assert.AreEqual(0, parsed.Content.Count);
+            Assert.AreEqual(LastResponseStatus.MissingParameters, parsed.Status);
+        }
+    }
+}
---
+++ src/IF.Lastfm.Core/Api/Commands/User/GetTopArtistsCommand.cs
@@ -0,0 +1,51 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+using IF.Lastfm.Core.Api.Enums;
+using IF.Lastfm.Core.Api.Helpers;
+using IF.Lastfm.Core.Objects;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace IF.Lastfm.Core.Api.Commands.User
+{
+    [ApiMethodName("user.getTopArtists")]
+    internal class GetTopArtistsCommand : GetAsyncCommandBase<PageResponse<LastArtist>>
+    {
+        public string Username { get; set; }
+        public LastStatsTimeSpan TimeSpan { get; set; }
+
+        public GetTopArtistsCommand(ILastAuth auth, string username, LastStatsTimeSpan span)
+            : base(auth)
+        {
+            Username = username;
+            TimeSpan = span;
+        }
+
+        public override void SetParameters()
+        {
+            Parameters.Add("user", Username);
+            Parameters.Add("period", TimeSpan.GetApiName());
+
+            AddPagingParameters();
+            DisableCaching();
+        }
+
+        public async override Task<PageResponse<LastArtist>> HandleResponse(HttpResponseMessage response)
+        {
+            var json = await response.Content.ReadAsStringAsync();
+
+            LastResponseStatus status;
+            if (LastFm.IsResponseValid(json, out status) && response.IsSuccessStatusCode)
+            {
+                var jtoken = JsonConvert.DeserializeObject<JToken>(json);
+                var topArtistsToken = jtoken.SelectToken("topartists");
+                var itemsToken = topArtistsToken.SelectToken("artist");
+                var pageInfoToken = topArtistsToken.SelectToken("@attr");
+
+                return PageResponse<LastArtist>.CreateSuccessResponse(itemsToken, pageInfoToken, LastArtist.ParseJToken, LastPageResultsType.Attr);
+            }
+
+            return LastResponse.CreateErrorResponse<PageResponse<LastArtist>>(status);
+        }
+    }
+}
