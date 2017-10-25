168c98cc66c2ccef1846ac7de8bbbfe27979509a
seafea <seafea@vt.edu>
2017-10-25 11:08:07 -0400

    Added GetLovedTracksCommand.cs
    Added GetLovedTracksCommand.cs Added json for testing it, and Unit
    Tests. Modified UserApi and IUserApi to add

---
+++ src/IF.Lastfm.Core/Api/Commands/User/GetLovedTracksCommand.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using IF.Lastfm.Core.Api.Enums;
+using IF.Lastfm.Core.Api.Helpers;
+using IF.Lastfm.Core.Objects;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace IF.Lastfm.Core.Api.Commands.User
+{
+    [ApiMethodName("user.getLovedTracks")]
+    internal class GetLovedTracksCommand : GetAsyncCommandBase<PageResponse<LastTrack>>
+    {
+        public string Username { get; set; }
+        public GetLovedTracksCommand(ILastAuth auth, string username) : base(auth)
+        {
+            Username = username;
+        }
+
+        public override void SetParameters()
+        {
+            Parameters.Add("user", Username);
+
+            this.AddPagingParameters();
+            this.DisableCaching();
+        }
+
+        public override async Task<PageResponse<LastTrack>> HandleResponse(HttpResponseMessage response)
+        {
+            var json = await response.Content.ReadAsStringAsync();
+
+            LastResponseStatus status;
+            if (LastFm.IsResponseValid(json, out status) && response.IsSuccessStatusCode)
+            {
+                var jtoken = JsonConvert.DeserializeObject<JToken>(json).SelectToken("lovedtracks");
+                var itemsToken = jtoken.SelectToken("track");
+                var attrToken = jtoken.SelectToken("@attr");
+
+                return PageResponse<LastTrack>.CreateSuccessResponse(
+                    itemsToken,
+                    attrToken,
+                    LastTrack.ParseJToken,
+                    LastPageResultsType.Attr);
+            }
+            else
+            {
+                return LastResponse.CreateErrorResponse<PageResponse<LastTrack>>(status);
+            }
+        }
+    }
+}
