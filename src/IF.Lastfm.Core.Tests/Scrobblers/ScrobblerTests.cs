014cd6565412bfc3b8154d448d96b5c32ff03b27
Johan Klinge <johan.klinge@gmail.com>
2018-12-03 17:56:17 +0100

    Added Scrobbler tests - still not working

---
+++ src/IF.Lastfm.Core.Tests/Scrobblers/ScrobblerTests.cs
@@ -0,0 +1,14 @@
+﻿using IF.Lastfm.Core.Scrobblers;
+using System.Net.Http;
+
+namespace IF.Lastfm.Core.Tests.Scrobblers
+{
+    public class ScrobblerTests : ScrobblerTestsBase
+    {
+        protected override ScrobblerBase GetScrobbler()
+        {
+            var httpClient = new HttpClient(FakeResponseHandler);
+            return new MemoryScrobbler(MockAuth.Object, httpClient);
+        }
+    }
+}
