450779a39f654fa203089f6159347d382c569f5f
Johan Klinge <johan.klinge@gmail.com>
2018-12-04 15:31:29 +0100

    ChartGetTopTagsTest - fix for bug? in the lastfm api chart.getTopArtists

--- src/IF.Lastfm.Core.Tests.Integration/Commands/ChartGetTopTagsTests.cs
+++ src/IF.Lastfm.Core.Tests.Integration/Commands/ChartGetTopTagsTests.cs
@@ -31,10 +31,13 @@ namespace IF.Lastfm.Core.Tests.Integration.Commands
         [Test]
         public async Task GetTopArtists_Success()
         {
-            var chartResponse = await Lastfm.Chart.GetTopArtistsAsync(PAGE, LIMIT);
+            //2018-12-04: there seems to be a bug in the lastfm api right now
+            //it is returning LIMIT*2 items if PAGE is anything but 1..
+            int PAGE1 = 1;
+            var chartResponse = await Lastfm.Chart.GetTopArtistsAsync(PAGE1, LIMIT);
 
             Assert.IsTrue(chartResponse.Success);
-            Assert.AreEqual(PAGE, chartResponse.Page, "Error in Page");
+            Assert.AreEqual(PAGE1, chartResponse.Page, "Error in Page");
             Assert.AreEqual(LIMIT, chartResponse.PageSize, "Error in PageSize");
             Assert.AreEqual(LIMIT, chartResponse.Content.Count, "Error in ContentCount");
 
