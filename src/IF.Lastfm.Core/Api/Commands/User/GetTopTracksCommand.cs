9120187a05f91b7cda0874ebff752a500c897f44
Aidan C <36613705+DigitalEntry@users.noreply.github.com>
2022-04-08 00:58:06 -0400

    Add files via upload
    Added GetTopTracksCommand

---
+++ src/IF.Lastfm.Core/Api/Commands/User/GetTopTracksCommand.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using IF.Lastfm.Core.Api.Enums;
+using IF.Lastfm.Core.Api.Helpers;
+using IF.Lastfm.Core.Objects;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace IF.Lastfm.Core.Api.Commands.User
+{
+    [ApiMethodName("user.getTopTracks")]
+    internal class GetTopTracksCommand : GetAsyncCommandBase<PageResponse<LastTrack>>
+    {
+        public string Username { get; private set; }
+
+        public DateTimeOffset? From { get; set; }
+
+        public DateTimeOffset? To { get; set; }
+
+        public bool? Extended { get; set; }
+
+        public GetTopTracksCommand(ILastAuth auth, string username) : base(auth)
+        {
+            Username = username;
+        }
+
+        public override void SetParameters()
+        {
+            Parameters.Add("user", Username);
+
+            if (From.HasValue)
+            {
+                Parameters.Add("from", From.Value.AsUnixTime().ToString());
+            }
+
+            if (To.HasValue)
+            {
+                Parameters.Add("to", To.Value.AsUnixTime().ToString());
+            }
+
+            if (Extended.HasValue)
+            {
+                Parameters.Add("extended", Extended.ToString());
+            }
+
+            AddPagingParameters();
+            DisableCaching();
+        }
+
+        public override async Task<PageResponse<LastTrack>> HandleResponse(HttpResponseMessage response)
+        {
+            var json = await response.Content.ReadAsStringAsync();
+
+            LastResponseStatus status;
+            if (LastFm.IsResponseValid(json, out status) && response.IsSuccessStatusCode)
+            {
+                var jtoken = JsonConvert.DeserializeObject<JToken>(json).SelectToken("toptracks");
+                var itemsToken = jtoken.SelectToken("track");
+                var attrToken = jtoken.SelectToken("@attr");
+
+                return PageResponse<LastTrack>.CreateSuccessResponse(itemsToken, attrToken, LastTrack.ParseJToken, LastPageResultsType.Attr);
+            }
+            else
+            {
+                return LastResponse.CreateErrorResponse<PageResponse<LastTrack>>(status);
+            }
+        }
+    }
+}
